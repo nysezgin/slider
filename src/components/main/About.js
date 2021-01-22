3f1b01313a67193ebc365515f241b83617ad1565
nysezgin <74066374+nysezgin@users.noreply.github.com>
2021-01-22 15:02:54 +0300

    deleted lorem, added meaningful data

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -10,9 +10,7 @@ export default function About() {
         <div className="about__info-wrapper">
           <h3 className="about__title">ABOUT OUR FURNITURE</h3>
           <p className="about__info">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            recusandae necessitatibus laboriosam laudantium inventore corporis,
-            architecto ut debitis deserunt soluta.
+            Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
           </p>
         </div>
         <img className="about__image" src={aboutImgLight} alt="" />
--- src/components/main/sliderData.js
+++ src/components/main/sliderData.js
@@ -12,7 +12,7 @@ const sliderData = [
     desktopImg: desktopHero1,
     title: "Discover innovative ways to decorate",
     info:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum ea, ratione quia officiis veniam, ipsam iste eum inventore necessitatibus beatae sed porro similique. Ullam pariatur totam, blanditiis accusamus perferendis repellendus.",
+      "We provide unmatched quality, comfort and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ const sliderData = [
     desktopImg: desktopHero2,
     title: "We are available all across the globe",
     info:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum ea, ratione quia officiis veniam, ipsam iste eum inventore necessitatibus beatae sed porro similique. Ullam pariatur totam, blanditiis accusamus perferendis repellendus.",
+      "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ const sliderData = [
     desktopImg: desktopHero3,
     title: "Manufactured with the best materials",
     info:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum ea, ratione quia officiis veniam, ipsam iste eum inventore necessitatibus beatae sed porro similique. Ullam pariatur totam, blanditiis accusamus perferendis repellendus.",
+      "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
   },
 ];
 
